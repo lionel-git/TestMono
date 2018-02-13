0ed269ada371f8b07f9343714e37f2335b7721cb
lionle-unix <lionel.desorme@free.fr>
2018-02-13 12:47:27 +0100

    Initial commit

---
+++ TestMono/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestMono
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine (Environment.OSVersion);
+		}
+	}
+}
